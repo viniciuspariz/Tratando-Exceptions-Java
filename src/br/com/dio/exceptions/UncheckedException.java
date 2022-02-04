30fb453585b18cf4f08afda354d2acc0124ca008
viniciuspariz <viniciuspariz@gmail.com>
2022-02-03 21:44:01 -0300

    Adicionando o try  catch finally

--- src/br/com/dio/exceptions/UncheckedException.java
+++ src/br/com/dio/exceptions/UncheckedException.java
@@ -5,15 +5,19 @@ import javax.swing.*;
 //Fazer a divisão de 2 valores inteiros
 public class UncheckedException {
     public static void main(String[] args) {
-
         String a = JOptionPane.showInputDialog("Numerador: ");
         String b = JOptionPane.showInputDialog("Denominador: ");
 
-        int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
-        System.out.println("Resultado: " + resultado);
+        try{
+            int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+            System.out.println("Resultado: " + resultado);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Chegou no Finally");
+        }
 
-
-        System.out.println("Chegou até aqui...");
+        System.out.println("O código continua...");
     }
 
     public static int dividir(int a, int b) {
