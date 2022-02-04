f680e64e4509bcc8c7dbe0843a678c08ce6bd759
viniciuspariz <viniciuspariz@gmail.com>
2022-02-03 23:08:58 -0300

    Corrigindo erros de sintaxe no UncheckedException

--- src/br/com/dio/exceptions/UncheckedException.java
+++ src/br/com/dio/exceptions/UncheckedException.java
@@ -5,24 +5,36 @@ import javax.swing.*;
 //Fazer a divisão de 2 valores inteiros
 public class UncheckedException {
     public static void main(String[] args) {
-        String a = JOptionPane.showInputDialog("Numerador: ");
-        String b = JOptionPane.showInputDialog("Denominador: ");
 
-        try {
-            int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
-            System.out.println("Resultado: " + resultado);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Entrada inválida, informe um número inteiro" + e.getMessage());
-        } catch (ArithmeticException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Impossível um número por 0" + e.getMessage());
-        } finally {
-            System.out.println("Chegou no Finally");
+        boolean continuelooping = true;
+
+        do {
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador: ");
+
+            try {
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+                System.out.println("Resultado: " + resultado);
+                continuelooping = false;
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Entrada inválida, informe um número inteiro" + e.getMessage());
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Impossível um número por 0" + e.getMessage());
+            } finally {
+                System.out.println("Chegou no Finally");
+            }
+
+            System.out.println("O código continua...");
+
+        } while (continuelooping);
+
+
+
         }
 
-        System.out.println("O código continua...");
-    }
+
 
     public static int dividir(int a, int b) {
         return a / b;
